1fb38bc427089881049bdd57d6ee0a930439b178
Nikolay Kozyrev <mikalai.kozyrau@symfa.com>
2023-12-07 23:20:10 +0300

    feat: init the project

---
+++ connections/src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig, isDevMode } from "@angular/core";
+import { provideRouter } from "@angular/router";
+import { provideEffects } from "@ngrx/effects";
+import { provideStore } from "@ngrx/store";
+import { provideStoreDevtools } from "@ngrx/store-devtools";
+
+import { routes } from "./app.routes";
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideStore(),
+    provideEffects(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+};
